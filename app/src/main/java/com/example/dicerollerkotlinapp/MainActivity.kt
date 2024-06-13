bcc9bc50fea2c6bd5e63fdb062966e4c75f7883c
batoul-alani <batoule50@gmail.com>
2024-06-13 23:56:53 +0300

    adding images to the app and roll dice to show the specific image

--- app/src/main/java/com/example/dicerollerkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerkotlinapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dicerollerkotlinapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -18,8 +19,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomId = Random.nextInt(6)+1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomId.toString()
+        val drawableResource = when (Random.nextInt(6)+1){
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else-> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.roll_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
